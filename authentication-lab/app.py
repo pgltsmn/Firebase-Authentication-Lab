c95853ac0de53bd8b301406b6b255f9b0fd2b317
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2022-07-31 23:27:23 +0300

    wqwdsfdd

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,17 +10,21 @@ config = {
   "messagingSenderId": "983001006387",
   "appId": "1:983001006387:web:34a15c3ce78c79543ebe46",
   "measurementId": "G-947TPG9FTR",
-  "databaseURL": ""
+  "databaseURL": "https://omgsmthcool-default-rtdb.firebaseio.com"
 };
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/')
 def gotosmth():
+    login_session['user'] = None
+    auth.current_user = None
+
     return render_template('home.html')
 
 @app.route('/signin', methods=['GET', 'POST'])
@@ -44,9 +48,15 @@ def signup():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
+        name = request.form['name']
+        username = request.form['username']
+        bio = request.form['bio']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect (url_for('add_tweet'))
+            user = {'email': email, 'password': password, 'name': name, 'username': username, 'bio': bio}
+            db.child('Users').child(login_session['user']['localId']).set(user)
+
+            return redirect(url_for('add_tweet'))
         except:
             error = "smth went wrong"
 
@@ -66,7 +76,7 @@ def add_tweet():
 #     login_session['user'] = None
 #     auth.current_user = None
 #     return redirect(url_for('home.html'))
-# return render_template('add_tweet.html')
+#     return render_template('add_tweet.html')
 
 
 
